50e4f924c8f0c67c867d36ab213040ee473426ed
engkqq <rizkimuhamadadamm@gmail.com>
2026-05-28 05:38:23 +0700

    Added 'Tabel For Dynamic Doors'

--- MySQL/database.sql
+++ MySQL/database.sql
@@ -27,6 +27,34 @@ CREATE TABLE IF NOT EXISTS `bans` (
   KEY `ucp` (`ucp`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- Dumping structure for table engkqopenmp.doors
+CREATE TABLE IF NOT EXISTS `doors` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT 'None',
+  `password` varchar(50) DEFAULT '',
+  `icon` int(11) DEFAULT 19130,
+  `locked` int(11) NOT NULL DEFAULT 0,
+  `admin` int(11) NOT NULL DEFAULT 0,
+  `vip` int(11) NOT NULL DEFAULT 0,
+  `faction` int(11) NOT NULL DEFAULT 0,
+  `family` int(11) NOT NULL DEFAULT -1,
+  `garage` tinyint(3) NOT NULL DEFAULT 0,
+  `custom` int(11) NOT NULL DEFAULT 0,
+  `extvw` int(11) DEFAULT 0,
+  `extint` int(11) DEFAULT 0,
+  `extposx` float DEFAULT 0,
+  `extposy` float DEFAULT 0,
+  `extposz` float DEFAULT 0,
+  `extposa` float DEFAULT 0,
+  `intvw` int(11) DEFAULT 0,
+  `intint` int(11) NOT NULL DEFAULT 0,
+  `intposx` float DEFAULT 0,
+  `intposy` float DEFAULT 0,
+  `intposz` float DEFAULT 0,
+  `intposa` float DEFAULT 0,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 -- Dumping structure for table engkqopenmp.player
 CREATE TABLE IF NOT EXISTS `player` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
